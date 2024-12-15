89b633380531ad804dd08437f08ed7e3074b3802
Norbert Koziana <koziana11norbert@gmail.com>
2024-12-15 22:54:37 +0100

    basic grid rendering and field updating

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public Image[,] board;
+
+        private Snake snake;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            InitializeSnake();
+
+            InitializeBoard();
+
+            DrawEmptyBoard();
+
+            test();
+        }
+
+        int i = 5;
+
+        private async void test()
+        {
+            await Task.Delay(3000);
+            board[i,i].Source = new BitmapImage(new Uri(@"./images/Fruit.png", UriKind.Relative));
+            i--;
+            if (i == 0)
+                return;
+            test();
+        }
+
+        private void InitializeSnake()
+        {
+            snake = new Snake();
+        }
+
+        private void InitializeBoard()
+        {
+            board = new Image[GameGrid.Rows, GameGrid.Columns];
+
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    Image imageField = new Image();
+                    imageField.Width = 50;
+                    GameGrid.Children.Add(imageField);
+                    board[i, j] = imageField;
+                }
+            }
+        }
+
+        public void DrawEmptyBoard()
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    board[i, j].Source = new BitmapImage(new Uri(@"./images/emptyField.jpg", UriKind.Relative));
+                }
+            }
+        }
+    }
+}
---
+++ Snake.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+    public class Snake
+    {
+        public LinkedList<Tuple<int, int>> currentLocation;
+        public Direction currentDirection;
+
+        public Snake()
+        {
+            this.currentLocation = new LinkedList<Tuple<int, int>>();
+            currentLocation.AddFirst(new Tuple<int, int>(3, 3));
+            this.currentDirection = Direction.Right;
+        }
+
+        public void MakeNextMove()//musi byc wywolywane np. co sekunde (powinno sie dac zmienic w opcjach jak czesto)
+        {
+            currentLocation.RemoveLast();
+
+            Tuple<int, int> headLocation = currentLocation.First();
+
+            Tuple<int, int> newHeadLocation = null;
+
+            switch (currentDirection)
+            {
+                case Direction.Left:
+                    newHeadLocation = new Tuple<int, int>(headLocation.Item1, headLocation.Item2 + 1);
+                    break;
+                case Direction.Right:
+                    newHeadLocation = new Tuple<int, int>(headLocation.Item1, headLocation.Item2 - 1);
+                    break;
+                case Direction.Top:
+                    newHeadLocation = new Tuple<int, int>(headLocation.Item1 + 1, headLocation.Item2);
+                    break;
+                default:
+                    newHeadLocation = new Tuple<int, int>(headLocation.Item1 - 1, headLocation.Item2);
+                    break;
+            }
+
+            //if (hasPlayerLost(newHeadLocation))
+            //{
+            //    return;
+            //}
+            //else if (hasPlayerGainedScore(newHeadLocation))
+            //{
+            //    generateNewFruit();
+            //}
+
+            currentLocation.AddFirst(newHeadLocation);
+        }
+    }
+}
